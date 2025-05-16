3b0abb00b4a277c969fc8bfea4bdca97a4bef4d7
Martin van IJcken <martin.vanijcken@tngtech.com>
2025-05-16 15:26:19 +0200

    Add my own information

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "Paul Käufl",
+    name: "Martin van IJcken",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["red, green and yellow", "fast feedback", "iterative development process"],
+    associations: ["red, green and yellow", "flaky e2e tests", "iterative development process"],
     // Which CI/CD tools do you use in your project?
     tools: "Jenkins",
     // What do you want to learn in this workshop?
-    expectations: ["How to improve the workshop.", "Which CI/CD Tools do you use in your projects?"],
+    expectations: [],
   },
 });
